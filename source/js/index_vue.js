0920904d5b2b3722a9390649ce3ac08806f97668
Han-chang Liu <39984251+Mini-ghost@users.noreply.github.com>
2019-05-16 00:22:58 +0800

    新增 vue 版本撥放器

---
+++ source/js/index_vue.js
@@ -0,0 +1,200 @@
+; (function () {
+
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame
+
+    window.requestAnimationFrame = requestAnimationFrame;
+    window.cancelAnimationFrame = cancelAnimationFrame;
+
+    window.isFullScreen = false
+
+})()
+    
+; (function () {
+    var u = navigator.userAgent;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+    var device = isAndroid || isiOS;
+    window.device = (function (device) {
+        if (device) {
+            if (isiOS) { return 'iOS' }
+            else { return 'Android' }
+        }
+        else {
+            return 'PC'
+        }
+    })(device)
+    
+})()
+    
+; (function () {
+    var vm = new Vue({
+        el: '#app',
+        data: function () {
+            return {
+                // 陣列
+                classData: new Array,
+                pathData: '',
+                speedArray: [1, 1.25, 1.5, 1.75, 2],
+
+                // 布林
+                isFullScreen: false,
+
+                // 數字
+                volume: 1,
+                speed: 1,
+
+                // 特殊
+                lastVolume: null,
+
+                // 字串
+                percentage: '',
+                durationTime: '00:00',
+                currentTime: '00:00'
+            }
+        },
+        created: function () {
+            $.ajax({
+                url: 'database/data.json',
+                method: 'get',
+                success: this.ajaxSuccessHandler
+            })
+        },
+        mounted: function () {
+            var player = this.dom.player
+
+            // 瀏覽器兼容設定
+            var requestFullscreen = player.requestFullscreen || player.webkitRequestFullScreen || player.mozRequestFullScreen  || player.msRequestFullscreen
+            player.requestFullscreen = requestFullscreen
+            
+            // 初始事件綁定
+            player.addEventListener('loadeddata', this.loadeddataHandler)
+        },
+        methods: {
+            ajaxSuccessHandler: function (res) {
+                this.classData = res;
+                this.pathData = this.classData[0].file
+
+            },
+            // 選單點擊
+            videoSelectHandler: function (path) {
+                this.pathData = path
+            },
+            videoPalyHandler: function (e) {
+                var target = e.target
+                var player = this.dom.player
+
+                if (this.isFullScreen && target === player) return
+                if (player.paused) {
+                    player.play();
+                    this.request = requestAnimationFrame(this.uploadTimeHandler);
+                }
+                else {
+                    player.pause();
+                    cancelAnimationFrame(this.request);
+                }
+            },
+            uploadTimeHandler: function () {
+                var player = this.dom.player
+                if (!player.paused) {
+                    // 更新播放時間
+                    this.currentTime = this.uploadTimeNowHandler(player.currentTime);
+                    // 更新播放進度
+                    this.percentage = (player.currentTime / player.duration) * 100
+                    this.request = requestAnimationFrame(this.uploadTimeHandler);
+                }
+            },
+            uploadTimeNowHandler: function (currentTime) {
+                var time, hh, mm, ss;
+                currentTime = ~~currentTime;
+                hh = ~~(currentTime / 3600);
+                mm = ~~((currentTime - (hh * 3600)) / 60);
+                ss = currentTime - (hh * 3600) - (mm * 60);
+
+                mm = mm > 9 ? mm : '0' + mm;
+                ss = ss > 9 ? ss : '0' + ss;
+                time = hh ? `${hh}:${mm}:${ss}` : `${mm}:${ss}`;
+                
+                return time;
+            },
+            uploadTimeAllHandler: function (duration) {
+                var time, hh, mm, ss;
+                duration = ~~duration;
+                hh = ~~(duration / 3600);
+                mm = ~~((duration - (hh * 3600)) / 60);
+                ss = duration - (hh * 3600) - (mm * 60);
+
+                mm = mm > 9 ? mm : '0' + mm;
+                ss = ss > 9 ? ss : '0' + ss;
+                time = hh ? `${hh}:${mm}:${ss}` : `${mm}:${ss}`;
+                
+                return time;
+            },
+            loadeddataHandler: function () {
+                var dom = this.dom
+                var player = dom.player
+                this.durationTime = this.uploadTimeAllHandler(player.duration)
+
+            },
+            volumeChangeHamdler: function () {
+                this.dom.player.volume = this.volume
+            },
+            fullScreenHandler: function () {
+                this.dom.player.requestFullscreen()
+            },
+            speedHandler: function () {
+                var speedArray = this.speedArray, player = this.dom.player;
+                switch(this.speed){
+                    case speedArray[0]:
+                        player.playbackRate = this.speed = speedArray[1]
+                        break;
+                    case speedArray[1]:
+                        player.playbackRate = this.speed = speedArray[2]
+                        break;
+                    case speedArray[2]:
+                        player.playbackRate = this.speed = speedArray[3]
+                        break;
+                    case speedArray[3]:
+                        player.playbackRate = this.speed = speedArray[4]
+                        break;
+                    case speedArray[4]:
+                        player.playbackRate = this.speed = speedArray[0]
+                        break;    
+                }
+                // this.dom.player.playbackRate = this.speed
+            },
+            volumeMuteHandler: function () {
+                var volume = this.dom.player.volume
+                if (volume) {
+                    this.lastVolume = volume
+                    this.dom.player.volume = this.volume = 0
+                }
+                else {
+                    this.dom.player.volume = this.volume = this.lastVolume;
+                }
+            },
+            upLoadProgressHandler: function (e) {
+                var dom = e.currentTarget;
+                var num = (e.offsetX / dom.offsetWidth), scrubTime;
+                scrubTime = num * this.dom.player.duration;
+                this.dom.player.currentTime = scrubTime;
+                this.currentTime = this.uploadTimeNowHandler(this.dom.player.currentTime);
+                this.percentage = num * 100;
+            }
+        },
+        computed: {
+            path: function () {
+                return `upload/${this.pathData}`
+            },
+            dom: function () {
+                return {
+                    player: document.getElementById('player'),
+                    nav: document.querySelector('.nav-unit'),
+                    timeNow: document.querySelector('.jsTimeNow'),
+                    timeAll: document.querySelector('.jsTimeAll'),
+                    progressFilled: document.querySelector('.jsProgressFilled')
+                }
+            }
+        }
+    })
+})()
